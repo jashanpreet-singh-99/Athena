4d28d3a7b6db74700706e339da92090f3ce4e087
jashanpreet-singh-99 <singh872@uwindsor.ca>
2023-06-15 01:40:17 -0400

    Added New Logo and Profile Icons
    Added App logo. Added border for the navbar buttons. Implemented area for notification and User profile Icon

--- Athena/urls.py
+++ Athena/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from Athena import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', views.Dash.as_view(), name='home_page')
 ]
+
+urlpatterns += staticfiles_urlpatterns()
