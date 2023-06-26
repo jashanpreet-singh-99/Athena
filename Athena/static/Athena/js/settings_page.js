4187f610354dac43c975806b3935313caa265bee
jashanpreet-singh-99 <singh872@uwindsor.ca>
2023-06-26 18:00:09 -0400

    FEAT: Basic Settings page
    On the settings page, the user can view the firstname lastname and edit those. In addition to tat user can view or edit profile photo. The Logic for profile phtoto is still pending. The username and email are also displayed but user can't change those since we will use those to generate user specific file logics.

--- Athena/static/Athena/js/settings_page.js
+++ Athena/static/Athena/js/settings_page.js
@@ -1,1 +1,28 @@
+console.log("Script is running");
 
+const editButton = document.getElementById('edit-button');
+    const readOnlyFields = document.getElementsByClassName('read-only');
+
+    editButton.addEventListener('click', function() {
+      document.querySelector('.details-container').classList.toggle('edit-mode');
+
+      if (editButton.innerHTML === 'Edit') {
+        editButton.innerHTML = 'Save';
+        enableFields();
+      } else {
+        editButton.innerHTML = 'Edit';
+        disableFields();
+      }
+    });
+
+    function enableFields() {
+      for (let field of readOnlyFields) {
+        field.removeAttribute('readonly');
+      }
+    }
+
+    function disableFields() {
+      for (let field of readOnlyFields) {
+        field.setAttribute('readonly', 'readonly');
+      }
+    }
