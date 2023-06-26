0608341a1dc9e63bf200fe1d6c1e46fc0a39eafe
jashanpreet-singh-99 <singh872@uwindsor.ca>
2023-06-26 17:58:05 -0400

    FEAT: Added Quick menu to profile
    Using the quick menu user can login or if the user is logged in the user can logout. In addition to that user can jump to the settings page if the user need to.But only if the user is already logged in.

--- Athena/static/Athena/js/nav_bar.js
+++ Athena/static/Athena/js/nav_bar.js
@@ -1,1 +1,37 @@
+console.log("Nav Script Loaded.");
+document.addEventListener('DOMContentLoaded', function() {
+    const profileContainer = document.getElementById('user_profile_btn');
+    const menu = document.getElementById('menu');
 
+    profileContainer.addEventListener('click', function(event) {
+      event.stopPropagation();
+      const buttonRect = profileContainer.getBoundingClientRect();
+        menu.style.display = 'block';
+        menu.style.top = buttonRect.bottom + 'px';
+        menu.style.left = (buttonRect.left - 80) + 'px';
+    });
+
+    document.addEventListener('click', function() {
+      menu.style.display = 'none';
+    });
+
+    // const menuItems = menu.querySelectorAll('li');
+    // menuItems.forEach(function(item) {
+    //     item.addEventListener('click', function(event) {
+    //       event.stopPropagation();
+    //       const menuItemText = item.textContent.trim();
+    //       // Perform actions based on the clicked menu item
+    //       if (menuItemText === 'Settings') {
+    //         // Perform action for 'Settings'
+    //         console.log('Settings clicked');
+    //       } else if (menuItemText === 'Logout') {
+    //         // Perform action for 'Logout'
+    //         console.log('Logout clicked');
+    //         var url = "{% url 'logout_page' %}"
+    //         document.location.href = url
+    //       }
+    //       // Close the menu
+    //       menu.style.display = 'none';
+    //     });
+    // });
+});
